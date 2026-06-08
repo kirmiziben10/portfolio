2683da13d9aac129ebdf7b923c87e55e5d4bf9f4
Yiğit Ziştoylu <yigitzistoylu@gmail.com>
2026-06-08 23:53:47 +0300

    Update snippet details page to the figma design.

--- src/pages/SnippetDetails.tsx
+++ src/pages/SnippetDetails.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import SandpackWindow from "../components/Sandpack";
+import backIcon from "../assets/back.svg";
 
 interface SnippetDetailsProps {
     isOpen: boolean;
@@ -10,6 +12,7 @@ interface SnippetDetailsProps {
 function SnippetDetails({ isOpen }: SnippetDetailsProps) {
     const containerRef = useRef<HTMLDivElement>(null);
     const isFirstRender = useRef(true);
+    const navigate = useNavigate();
 
     useGSAP(() => {
         const currentScrollY = window.scrollY;
@@ -23,9 +26,9 @@ function SnippetDetails({ isOpen }: SnippetDetailsProps) {
             }
             else { gsap.set(containerRef.current, { y: startY, display: "none" }) };
 
-            return() => {
-            isFirstRender.current = true;
-          };
+            return () => {
+                isFirstRender.current = true;
+            };
         }
 
         if (isOpen) {
@@ -38,7 +41,7 @@ function SnippetDetails({ isOpen }: SnippetDetailsProps) {
                     duration: 0.5,
                     ease: "back.out(1)"
                 }
-            )
+            );
         }
 
         if (!isOpen) {
@@ -50,16 +53,56 @@ function SnippetDetails({ isOpen }: SnippetDetailsProps) {
                     ease: "back.in(1)",
                     display: "none"
                 }
-            )
+            );
         }
 
     }, { scope: containerRef, dependencies: [isOpen] });
 
     return (
         <main ref={containerRef} className="snippet-details">
-            <h1>Hello Snippet</h1>
+            <div className="snippet-details-header">
+                <button 
+                    className="back-button" 
+                    onClick={() => navigate("/")} 
+                    aria-label="Go Back"
+                >
+                    <img
+                        className="back-icon"
+                        src={backIcon}
+                        alt="Go Back"
+                    />
+                </button>
+                <div className="snippet-name-container">
+                    <span className="snippet-name-text">Reveal</span>
+                </div>
+            </div>
+            <div className="snippet-details-info">
+                <div className="snippet-display-wrapper">
+                    <SandpackWindow />
+                </div>
+                <div className="snippet-details-right">
+                    <div className="snippet-about">
+                        <span className="about-title">About Snippet</span>
+                        <p className="about-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum turpis urna, at posuere elit mattis vitae. Pellentesque tincidunt ullamcorper vulputate. Vestibulum varius porta nisl id vehicula. Integer velit nisl, venenatis id eros molestie, pharetra semper massa. Pellentesque pretium vehicula lorem, sed sollicitudin purus gravida et. Nam lacus metus, fringilla et diam nec, dapibus molestie nunc. Integer vel congue odio.
+                        </p>
+                    </div>
+                    <div className="snippet-bottom">
+                        <div className="snippet-categories">
+                            <span className="category-badge badge-interactive">Interactive</span>
+                            <span className="category-badge badge-animation">Animation</span>
+                            <span className="category-badge badge-svg">SVG</span>
+                        </div>
+                        <div className="snippet-options">
+                            <button className="option-button open-codepen">
+                                Open in CodePen
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
-    )
+    );
 }
 
 export default SnippetDetails;
